a178bda4d5da3056d973bfaf29a9a787ec0ac51f
EduFed <EduFed@users.noreply.github.com>
2026-08-11 15:17:21 +0300

    2026-08-10-13: способ работы вместо прав - работает вместе, по наряду, только смотрит

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    старый index.html из кэша.
    ============================================================ */
 
-const CACHE_VERSION = '2026-08-10-12';
+const CACHE_VERSION = '2026-08-10-13';
 
 const CACHE_APP = 'smeta-app-' + CACHE_VERSION;   // файлы приложения — своя версия у каждой сборки
 
